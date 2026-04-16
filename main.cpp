2486ba9874eeaee228f1ba0d5601983c508e5757
jigneshkotia <kotiajignesh8@gmail.com>
2026-04-16 13:22:35 +0530

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <cstdlib>
 #include <string>
+#include <vector>
 
 #include "json.hpp"
 #include "blockchain.h"
@@ -13,14 +14,11 @@
 using json = nlohmann::json;
 using namespace std;
 
-// Global blockchain
 Blockchain my_blockchain;
-
 
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // FIXED PORT HANDLING
     int port = 8080;
     char* port_env = getenv("PORT");
     if (port_env != nullptr) {
@@ -35,7 +33,6 @@ int main() {
     int opt = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    // ERROR CHECKING
     if (::bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
@@ -50,19 +47,37 @@ int main() {
 
     while (true) {
         int client = accept(server_fd, nullptr, nullptr);
+        if (client < 0) continue;
 
-        char buffer[8192] = {0};
-        int valread = read(client, buffer, sizeof(buffer));
+        // --- IMPROVED READING LOGIC ---
+        vector<char> full_buffer(8192);
+        int valread = read(client, full_buffer.data(), full_buffer.size());
 
         if (valread <= 0) {
             close(client);
             continue;
         }
 
-        string req(buffer);
-        cout << "Request:\n" << req << endl;
+        string req(full_buffer.data(), valread);
+        
+        // If it's a POST/PUT, we might need to read more if the body is cut off
+        size_t header_end = req.find("\r\n\r\n");
+        size_t cl_pos = req.find("Content-Length: ");
+        
+        if (cl_pos != string::npos && header_end != string::npos) {
+            size_t cl_end = req.find("\r\n", cl_pos);
+            int content_length = stoi(req.substr(cl_pos + 16, cl_end - (cl_pos + 16)));
+            
+            // While we have less data than headers + body, keep reading
+            while ((req.length() - (header_end + 4)) < content_length) {
+                char temp[1024] = {0};
+                int n = read(client, temp, sizeof(temp));
+                if (n <= 0) break;
+                req.append(temp, n);
+            }
+        }
+        // ------------------------------
 
-        // Parse method & path
         size_t first_space = req.find(' ');
         size_t second_space = req.find(' ', first_space + 1);
 
@@ -74,18 +89,15 @@ int main() {
         string method = req.substr(0, first_space);
         string path = req.substr(first_space + 1, second_space - first_space - 1);
 
-        // Handle CORS preflight
         if (method == "OPTIONS") {
-            string response = ServerUtils :: build_response("");
+            string response = ServerUtils::build_response("");
             send(client, response.c_str(), response.size(), 0);
             close(client);
             continue;
         }
 
-        // Extract JSON body
-        json body = ServerUtils :: extract_json(req);
+        json body = ServerUtils::extract_json(req);
 
-        // Route request
         string response_body;
         try {
             response_body = handle_request(path, method, body);
@@ -93,8 +105,7 @@ int main() {
             response_body = R"({"error": "Something went wrong"})";
         }
 
-        // Send response
-        string full_response = ServerUtils :: build_response(response_body);
+        string full_response = ServerUtils::build_response(response_body);
         send(client, full_response.c_str(), full_response.size(), 0);
 
         close(client);
